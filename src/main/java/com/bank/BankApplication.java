ae7039342dffa05ade3ac497f9f2ee046c64b3d5
nti14nti <nti14nti@gmail.com>
2026-05-19 14:42:22 +0200

    add Exceptions

--- src/main/java/com/bank/BankApplication.java
+++ src/main/java/com/bank/BankApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankApplication {
-<<<<<<< HEAD
 
-=======
->>>>>>> 475c67622ae2409880929e6ad936c87e27dabe6c
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
     }
